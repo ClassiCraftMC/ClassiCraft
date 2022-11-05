7e268b8ec72f5e775608713ff8f0cfdf696fd1e5
1798643961 <1798643961@qq.com>
2022-11-05 22:37:20 +0800

    更新盐结晶岩

--- src/main/java/nameless/classicraft/ClassiCraftConfiguration.java
+++ src/main/java/nameless/classicraft/ClassiCraftConfiguration.java
@@ -6,10 +6,24 @@ public class ClassiCraftConfiguration {
 
     public static final ForgeConfigSpec SPEC;
     public static final ForgeConfigSpec.Builder BUILDER = new ForgeConfigSpec.Builder();
+    public static final ForgeConfigSpec.IntValue WEAKNESS_EFFECT_AMPLIFIER;
+    public static final ForgeConfigSpec.DoubleValue WATER_REDUCING_RATE;
+    public static final ForgeConfigSpec.BooleanValue RESET_WATER_LEVEL_IN_DEATH;
 
     static {
-        String desc;
         BUILDER.comment("天工开物模组配置文件");
+        BUILDER.push("General");
+        WEAKNESS_EFFECT_AMPLIFIER =
+                BUILDER.comment("  ", "It is the weakness effect amplifier of the effect punishment when player's water level is too low. -1 means canceling this effect. Default:0")
+                        .defineInRange("weaknessEffectAmplifier", 0, -1, 999999);
+
+        WATER_REDUCING_RATE =
+                BUILDER.comment(" ", "finalReducingValue = basicValue * waterReducingRate.(DoubleValue)", "Default:1.0")
+                        .defineInRange("waterReducingRate", 1.0D, 0d, 1000D);
+
+        RESET_WATER_LEVEL_IN_DEATH =
+                BUILDER.comment(" ", "It decides if players' water level would reset in death.", "Default:true")
+                        .define("resetWaterLevelInDeath", true);
         BUILDER.pop();
         SPEC = BUILDER.build();
     }
