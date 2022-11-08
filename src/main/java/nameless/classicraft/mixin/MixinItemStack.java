3e91bfdf208c37976472897e917e243472298a67
1798643961 <1798643961@qq.com>
2022-11-08 23:11:00 +0800

    修复Mixin错误

--- src/main/java/nameless/classicraft/mixin/MixinItemStack.java
+++ src/main/java/nameless/classicraft/mixin/MixinItemStack.java
@@ -2,23 +2,19 @@ package nameless.classicraft.mixin;
 
 import nameless.classicraft.api.item.CCItemStack;
 import nameless.classicraft.capability.ModCapabilities;
-import net.minecraft.nbt.CompoundTag;
 import net.minecraft.network.chat.Component;
 import net.minecraft.network.chat.Style;
 import net.minecraft.world.entity.SlotAccess;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.inventory.ClickAction;
 import net.minecraft.world.inventory.Slot;
-import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.Items;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
-import javax.annotation.Nullable;
 import java.util.Objects;
 
 /**
@@ -27,18 +23,11 @@ import java.util.Objects;
 @Mixin(value = ItemStack.class)
 public abstract class MixinItemStack implements CCItemStack {
 
-    @Shadow public abstract boolean isEmpty();
-    @Shadow public abstract Item getItem();
-    @Shadow public abstract int getCount();
-    @Shadow public abstract int getMaxStackSize();
-    @Shadow public abstract void grow(int pIncrement);
-    @Shadow @Nullable public abstract CompoundTag getTag();
-
     @Inject(method = "overrideOtherStackedOnMe", cancellable = true, at = @At("HEAD"))
     private void overrideOtherStackedOnMeCC(ItemStack other, Slot pSlot, ClickAction pAction,
                                             Player pPlayer, SlotAccess pAccess, CallbackInfoReturnable<Boolean> cir) {
 
-        if (other.isEmpty() || isEmpty() || getItem() != other.getItem() || !Objects.equals(getTag(), other.getTag())) {
+        if (other.isEmpty() || (((ItemStack) (Object) this).isEmpty() || (((ItemStack) (Object) this).getItem() != other.getItem() || !Objects.equals((((ItemStack) (Object) this).getTag()), other.getTag())))) {
             cir.setReturnValue(false);
             return;
         }
@@ -47,12 +36,13 @@ public abstract class MixinItemStack implements CCItemStack {
             int count = other.getCount();
 
             getCapability(ModCapabilities.ROT).ifPresent(rotSelf -> {
-                int selfCount = getCount();
-                int finalCount = Math.min(count, getMaxStackSize() - selfCount);
+                ItemStack itemStack = ((ItemStack) (Object) this);
+                int selfCount = (((ItemStack) (Object) this).getCount());
+                int finalCount = Math.min(count, (((ItemStack) (Object) this).getMaxStackSize() - selfCount));
                 var finalRot = (rotOther.getRotValue() - rotSelf.getRotValue()) * finalCount / (selfCount + finalCount);
 
                 other.shrink(finalCount);
-                grow(finalCount);
+                itemStack.grow(finalCount);
 
                 rotSelf.setRotValue(finalRot + rotSelf.getRotValue());
 
@@ -64,7 +54,7 @@ public abstract class MixinItemStack implements CCItemStack {
     @Inject(method = "getHoverName", at = @At("RETURN"), cancellable = true)
     private void getHoverNameCC(CallbackInfoReturnable<Component> cir) {
         getCapability(ModCapabilities.ROT).ifPresent(rot -> {
-            if (rot.getHolder().getMax() > 0 && this.getItem() != Items.ROTTEN_FLESH) {
+            if (rot.getHolder().getMax() > 0 && (((ItemStack) (Object) this).getItem() != Items.ROTTEN_FLESH)) {
                 Component origin = cir.getReturnValue();
                 cir.setReturnValue(rot.getLevelName().append(origin)
                         .setStyle(Style.EMPTY.withItalic(false)
