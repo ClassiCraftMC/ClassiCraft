58c58d9fc66079385c388bd281f6363dd15d704e
1798643961 <1798643961@qq.com>
2022-11-15 11:54:32 +0800

    水瓶熄灭变木棍

--- src/main/java/nameless/classicraft/event/TestEvents.java
+++ src/main/java/nameless/classicraft/event/TestEvents.java
@@ -6,9 +6,12 @@ import nameless.classicraft.block.realistic.RealisticTorchBlock;
 import nameless.classicraft.init.ModBlockProperties;
 import nameless.classicraft.init.ModBlocks;
 import net.minecraft.world.entity.Entity;
+import net.minecraft.world.entity.item.ItemEntity;
 import net.minecraft.world.entity.projectile.Projectile;
 import net.minecraft.world.entity.projectile.ThrownPotion;
+import net.minecraft.world.item.Items;
 import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.Blocks;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.eventbus.api.IEventBus;
 
@@ -23,24 +26,20 @@ public class TestEvents {
         Block block = event.getHitBlock();
         Projectile projectile = event.getEntity();
         if ((block != null
-                && projectile.isInFluidType()
-                && block.defaultBlockState().is(ModBlocks.TORCH.get())
-                /**&& block.defaultBlockState().getValue(RealisticTorchBlock.getLitState())
-                != RealisticTorchBlock.UNLIT*/)) {
-            ClassiCraftMod.LOGGER.info("Test 1");
-            ModBlockProperties.playExtinguishSound(projectile.getLevel(), projectile.getOnPos());
-            projectile.getLevel().setBlockAndUpdate(projectile.getOnPos(),
-                    ModBlocks.TORCH.get().defaultBlockState());
-        }
-        if ((block != null
                 && projectile instanceof ThrownPotion
                 && block.defaultBlockState().is(ModBlocks.TORCH.get())
-                /**&& block.defaultBlockState().getValue(RealisticTorchBlock.getLitState())
-                 != RealisticTorchBlock.UNLIT*/)) {
+                && block.defaultBlockState().getValue(RealisticTorchBlock.getLitState())
+                 != RealisticTorchBlock.UNLIT)) {
             ClassiCraftMod.LOGGER.info("Test 2");
             ModBlockProperties.playExtinguishSound(projectile.getLevel(), projectile.getOnPos());
             projectile.getLevel().setBlockAndUpdate(projectile.getOnPos(),
-                    ModBlocks.TORCH.get().defaultBlockState());
+                    Blocks.AIR.defaultBlockState());
+            ItemEntity newItem = new ItemEntity(
+                    projectile.getLevel(),
+                    projectile.getX(), projectile.getY(),
+                    projectile.getZ(),
+                    Items.STICK.getDefaultInstance());
+            projectile.getLevel().addFreshEntity(newItem);
         }
 
     }
