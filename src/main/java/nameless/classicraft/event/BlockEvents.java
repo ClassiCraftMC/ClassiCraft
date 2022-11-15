65f4664541541b39b453174d5362df1b481e82db
1798643961 <1798643961@qq.com>
2022-11-15 22:48:47 +0800

    测试坩埚

---
+++ src/main/java/nameless/classicraft/event/BlockEvents.java
@@ -0,0 +1,40 @@
+package nameless.classicraft.event;
+
+import nameless.classicraft.ClassiCraftMod;
+import nameless.classicraft.api.event.PlayerRightClickBlockEvent;
+import net.minecraft.sounds.SoundEvents;
+import net.minecraft.sounds.SoundSource;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.Items;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.Blocks;
+import net.minecraft.world.level.block.CauldronBlock;
+import net.minecraft.world.level.block.LayeredCauldronBlock;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+
+@Mod.EventBusSubscriber
+public class BlockEvents {
+
+    @SubscribeEvent
+    public static void onCauldronInteract(PlayerRightClickBlockEvent event) {
+        Block block = event.getBlock();
+        Player player = event.getEntity();
+        ItemStack itemStack = player.getMainHandItem();
+        if (block instanceof CauldronBlock) {
+            ClassiCraftMod.LOGGER.info("Test!");
+            if (itemStack.is(Items.BLACK_WOOL)
+                    && block.defaultBlockState().getValue(LayeredCauldronBlock.LEVEL) == 3) {
+                itemStack.split(1);
+                ItemStack newItem = new ItemStack(Items.WHITE_WOOL);
+                player.getInventory().add(newItem);
+                event.getLevel().setBlockAndUpdate(
+                        event.getPos(),
+                        block.defaultBlockState().setValue(LayeredCauldronBlock.LEVEL, 2)
+                );
+                event.getLevel().playSound(null, event.getPos(), SoundEvents.BUCKET_EMPTY, SoundSource.BLOCKS, 1.0F, event.getLevel().getRandom().nextFloat() * 0.1F + 0.9F);
+            }
+        }
+    }
+}
