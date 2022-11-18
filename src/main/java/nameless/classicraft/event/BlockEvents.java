a2f224796e4a51bb3fadb9087383652c40505300
1798643961 <1798643961@qq.com>
2022-11-18 16:41:32 +0800

    恢复部分

--- src/main/java/nameless/classicraft/event/BlockEvents.java
+++ src/main/java/nameless/classicraft/event/BlockEvents.java
@@ -1,19 +1,29 @@
 package nameless.classicraft.event;
 
+import nameless.classicraft.ClassiCraftConfiguration;
 import nameless.classicraft.api.event.PlayerRightClickBlockEvent;
 import nameless.classicraft.api.light.LightAPI;
 import nameless.classicraft.block.realistic.*;
 import nameless.classicraft.init.ModBlocks;
+import nameless.classicraft.init.ModItems;
+import net.minecraft.network.chat.Component;
+import net.minecraft.sounds.SoundEvents;
+import net.minecraft.sounds.SoundSource;
+import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.item.ItemEntity;
+import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.entity.projectile.Projectile;
 import net.minecraft.world.entity.projectile.ThrownPotion;
+import net.minecraft.world.item.Item;
 import net.minecraft.world.item.Items;
 import net.minecraft.world.level.Level;
+import net.minecraft.world.level.LevelAccessor;
 import net.minecraft.world.level.block.*;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.gameevent.GameEvent;
 import net.minecraft.world.phys.HitResult;
 import net.minecraftforge.event.entity.ProjectileImpactEvent;
+import net.minecraftforge.event.level.BlockEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 
@@ -223,5 +233,56 @@ public class BlockEvents {
                 LightAPI.playExtinguishSound(event.getLevel(), event.getPos());
             }
         }
+    }
+
+    @SubscribeEvent
+    public static void stopBlockPlace(BlockEvent.EntityPlaceEvent event) {
+        Entity entity = event.getEntity();
+        Block block = event.getPlacedBlock().getBlock();
+        LevelAccessor level = event.getLevel();
+        Item item = block.asItem();
+        if (entity instanceof Player && block instanceof TorchBlock
+                && !item.getDefaultInstance().is(Items.REDSTONE_TORCH)
+                && !item.getDefaultInstance().is(Items.SOUL_TORCH)
+                && ClassiCraftConfiguration.noVanillaTorchPlace.get()) {
+            if (!((Player) entity).isCreative()) {
+                level.playSound(null, event.getPos(), SoundEvents.WOOD_PLACE, SoundSource.BLOCKS, 1, 1);
+                level.setBlock(event.getPos(), ModBlocks.TORCH.get().defaultBlockState(), 1);
+                entity.sendSystemMessage(Component.translatable("info.classicraft.stop_use_torch"));
+            }
+        }
+        if (entity instanceof Player && block instanceof TorchBlock
+                && !item.getDefaultInstance().is(Items.REDSTONE_TORCH)
+                && !item.getDefaultInstance().is(Items.TORCH)
+                && ClassiCraftConfiguration.noVanillaTorchPlace.get()) {
+            if (!((Player) entity).isCreative()) {
+                level.playSound(null, event.getPos(), SoundEvents.WOOD_PLACE, SoundSource.BLOCKS, 1, 1);
+                level.setBlock(event.getPos(), ModBlocks.SOUL_TORCH.get().defaultBlockState(), 1);
+                entity.sendSystemMessage(Component.translatable("info.classicraft.stop_use_torch"));
+            }
+        }
+        /**
+        if (entity instanceof Player
+                && block instanceof LanternBlock
+                && !item.getDefaultInstance().is(Items.SOUL_LANTERN)
+                && !item.getDefaultInstance().is(ModItems.SOUL_LANTERN.get().asItem())
+                && !item.getDefaultInstance().is(ModItems.LIT_SOUL_LANTERN.get())
+                && ClassiCraftConfiguration.noVanillaLanternPlace.get()) {
+            if (!((Player) entity).isCreative()) {
+                level.playSound(null, event.getPos(), SoundEvents.LANTERN_PLACE, SoundSource.BLOCKS, 1, 1);
+                level.setBlock(event.getPos(), ModBlocks.LANTERN.get().defaultBlockState(), 1);
+            }
+        }
+        if (entity instanceof Player
+                && block instanceof LanternBlock
+                && !item.getDefaultInstance().is(Items.LANTERN)
+                && !item.getDefaultInstance().is(ModItems.LANTERN.get().asItem())
+                && !item.getDefaultInstance().is(ModItems.LIT_LANTERN.get())
+                && ClassiCraftConfiguration.noVanillaLanternPlace.get()) {
+            if (!((Player) entity).isCreative()) {
+                level.playSound(null, event.getPos(), SoundEvents.LANTERN_PLACE, SoundSource.BLOCKS, 1, 1);
+                level.setBlock(event.getPos(), ModBlocks.SOUL_LANTERN.get().defaultBlockState(), 1);
+            }
+        }*/
     }
 }
