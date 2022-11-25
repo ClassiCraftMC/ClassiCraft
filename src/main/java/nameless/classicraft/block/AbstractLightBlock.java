13831fc4bde0aacfd1bc0e555119c3e4e8a8d5e1
1798643961 <1798643961@qq.com>
2022-11-25 11:40:50 +0800

    重写火把方块

--- src/main/java/nameless/classicraft/block/AbstractLightBlock.java
+++ src/main/java/nameless/classicraft/block/AbstractLightBlock.java
@@ -1,12 +1,8 @@
 package nameless.classicraft.block;
 
 import net.minecraft.world.level.block.Block;
-import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.block.state.properties.BlockStateProperties;
 import net.minecraft.world.level.block.state.properties.BooleanProperty;
-import net.minecraft.world.level.block.state.properties.IntegerProperty;
-
-import java.util.function.ToIntFunction;
 
 /**
  * Abstract Light Block
@@ -19,26 +15,6 @@ public abstract class AbstractLightBlock extends Block {
     public AbstractLightBlock(Properties pProperties) {
         super(pProperties);
     }
-
-    /**
-     * A method to switch light value
-     * @return int value about light emmison
-     */
-    static ToIntFunction<BlockState> getLightFromState(){
-        return null;
-    }
-
-    /**
-     * A method to get burn time
-     * @return burn time
-     */
-    abstract IntegerProperty getBurnTime();
-
-    /**
-     * A method to get initial burn time
-     * @return initial burn time
-     */
-    abstract int getInitialBurnTime();
 
     /**
      * A method to get Lit State
--- src/main/java/nameless/classicraft/block/RealTorchBlock.java
+++ src/main/java/nameless/classicraft/block/RealTorchBlock.java
@@ -1,16 +1,35 @@
 package nameless.classicraft.block;
 
 import nameless.classicraft.init.ModBlockProperties;
+import net.minecraft.MethodsReturnNonnullByDefault;
+import net.minecraft.core.BlockPos;
+import net.minecraft.core.Direction;
+import net.minecraft.core.particles.ParticleTypes;
+import net.minecraft.util.RandomSource;
+import net.minecraft.world.level.BlockGetter;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.level.LevelAccessor;
+import net.minecraft.world.level.LevelReader;
 import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.block.SoundType;
 import net.minecraft.world.level.block.state.BlockBehaviour;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.block.state.StateDefinition;
 import net.minecraft.world.level.block.state.properties.IntegerProperty;
 import net.minecraft.world.level.material.Material;
+import net.minecraft.world.phys.shapes.CollisionContext;
+import net.minecraft.world.phys.shapes.VoxelShape;
 
+import javax.annotation.ParametersAreNonnullByDefault;
+import java.util.function.ToIntFunction;
+
+@MethodsReturnNonnullByDefault
+@ParametersAreNonnullByDefault
 public class RealTorchBlock extends AbstractLightBlock {
 
+    protected static final int AABB_STANDING_OFFSET = 2;
+    protected static final VoxelShape AABB = Block.box(6.0D, 0.0D, 6.0D, 10.0D, 10.0D, 10.0D);
     protected static final IntegerProperty TORCH_BURN_TIME = ModBlockProperties.TORCH_BURN_TIME;
     protected static final int TORCH_INITIAL_BURN_TIME = ModBlockProperties.TORCH_INITIAL_BURN_TIME;
 
@@ -20,17 +39,57 @@ public class RealTorchBlock extends AbstractLightBlock {
     }
 
     @Override
+    public VoxelShape getShape(BlockState pState, BlockGetter pLevel, BlockPos pPos, CollisionContext pContext) {
+        return AABB;
+    }
+
+    /**
+     * Update the provided state given the provided neighbor direction and neighbor state, returning a new state.
+     * For example, fences make their connections to the passed in state if possible, and wet concrete powder immediately
+     * returns its solidified counterpart.
+     * Note that this method should ideally consider only the specific direction passed in.
+     */
+    @Override
+    public BlockState updateShape(BlockState pState, Direction pFacing, BlockState pFacingState, LevelAccessor pLevel, BlockPos pCurrentPos, BlockPos pFacingPos) {
+        return pFacing == Direction.DOWN && !this.canSurvive(pState, pLevel, pCurrentPos) ? Blocks.AIR.defaultBlockState() : super.updateShape(pState, pFacing, pFacingState, pLevel, pCurrentPos, pFacingPos);
+    }
+
+    @Override
+    public boolean canSurvive(BlockState pState, LevelReader pLevel, BlockPos pPos) {
+        return canSupportCenter(pLevel, pPos.below(), Direction.UP);
+    }
+
+    @Override
+    public void animateTick(BlockState pState, Level pLevel, BlockPos pPos, RandomSource pRandom) {
+        if (pState.getValue(LIT)) {
+            double d0 = (double)pPos.getX() + 0.5D;
+            double d1 = (double)pPos.getY() + 0.7D;
+            double d2 = (double)pPos.getZ() + 0.5D;
+            pLevel.addParticle(ParticleTypes.SMOKE, d0, d1, d2, 0.0D, 0.0D, 0.0D);
+            pLevel.addParticle(ParticleTypes.FLAME, d0, d1, d2, 0.0D, 0.0D, 0.0D);
+        }
+    }
+
+    private static ToIntFunction<BlockState> getLightFromState(){
+        return (state) -> {
+            if (state.getValue(LIT)) {
+                return 17;
+            }else {
+                return 0;
+            }
+        };
+    }
+
+    @Override
     protected void createBlockStateDefinition(StateDefinition.Builder<Block, BlockState> pBuilder) {
         pBuilder.add(LIT, TORCH_BURN_TIME);
     }
 
-    @Override
-   IntegerProperty getBurnTime() {
+   public static IntegerProperty getBurnTime() {
         return TORCH_BURN_TIME;
     }
 
-    @Override
-    int getInitialBurnTime() {
+    public static int getInitialBurnTime() {
         return TORCH_INITIAL_BURN_TIME;
     }
 }
