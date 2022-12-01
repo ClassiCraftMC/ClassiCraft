7acd76acd2473c959d867fbef0d27456a9c48954
1798643961 <1798643961@qq.com>
2022-12-01 13:21:30 +0800

    增加替换

--- src/main/java/nameless/classicraft/levelgen/ReplaceAllFeatureModifier.java
+++ src/main/java/nameless/classicraft/levelgen/ReplaceAllFeatureModifier.java
@@ -10,9 +10,11 @@ import net.minecraftforge.common.world.BiomeModifier;
 import net.minecraftforge.common.world.ModifiableBiomeInfo;
 
 public record ReplaceAllFeatureModifier(Holder<PlacedFeature> feature) implements BiomeModifier {
+
     public void modify(Holder<Biome> biome, Phase phase, ModifiableBiomeInfo.BiomeInfo.Builder builder) {
         if (phase == Phase.AFTER_EVERYTHING) {
             builder.getGenerationSettings().addFeature(GenerationStep.Decoration.TOP_LAYER_MODIFICATION, feature);
+            builder.getGenerationSettings().addFeature(GenerationStep.Decoration.VEGETAL_DECORATION, feature);
         }
     }
 
