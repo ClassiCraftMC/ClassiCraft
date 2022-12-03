e18d2d27620329e960fec534332acdf38067044c
1798643961 <1798643961@qq.com>
2022-12-03 22:39:19 +0800

    Test

---
+++ src/main/java/nameless/classicraft/event/TestEvents.java
@@ -0,0 +1,31 @@
+package nameless.classicraft.event;
+
+import nameless.classicraft.api.event.ItemEntityTickEvent;
+import net.minecraft.world.entity.item.ItemEntity;
+import net.minecraft.world.item.Items;
+import net.minecraftforge.common.Tags;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+import sereneseasons.api.season.ISeasonState;
+import sereneseasons.api.season.Season;
+import sereneseasons.handler.season.SeasonHandler;
+import sereneseasons.season.SeasonSavedData;
+
+@Mod.EventBusSubscriber
+public class TestEvents {
+
+    @SubscribeEvent
+    public static void changeLooseRock(ItemEntityTickEvent event) {
+        ItemEntity entity = event.getEntity();
+        if (((ISeasonState) entity.getLevel()).getSeason()
+                == Season.SubSeason.EARLY_AUTUMN.getSeason()) {
+            if (entity.getItem().is(Items.BONE)) {
+                if (entity.getItem().getCount() == 2) {
+                    if (entity.getLevel().getBiome(entity.getOnPos()).is(Tags.Biomes.IS_HOT)) {
+                        //TODO
+                    }
+                }
+            }
+        }
+    }
+}
