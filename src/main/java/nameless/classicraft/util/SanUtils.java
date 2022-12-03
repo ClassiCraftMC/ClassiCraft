b30f4752ec370778f76bebca2a60e65f2c399319
1798643961 <1798643961@qq.com>
2022-12-03 17:17:14 +0800

    San Bar and San value

---
+++ src/main/java/nameless/classicraft/util/SanUtils.java
@@ -0,0 +1,14 @@
+package nameless.classicraft.util;
+
+import nameless.classicraft.api.san.ISanHandler;
+import net.minecraft.client.Minecraft;
+import net.minecraft.world.entity.player.Player;
+
+public class SanUtils {
+
+    public static boolean checkSanExist() {
+        Player player = Minecraft.getInstance().player;
+        assert player != null;
+        return ((ISanHandler) player).getSan() > 0;
+    }
+}
