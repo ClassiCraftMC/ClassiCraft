3f2a233c5b6841e6c63be367241abd505cc9303e
1798643961 <1798643961@qq.com>
2022-12-05 19:22:09 +0800

    Food Tooltip

--- src/main/java/nameless/classicraft/event/ItemEvent.java
+++ src/main/java/nameless/classicraft/event/ItemEvent.java
@@ -2,19 +2,47 @@ package nameless.classicraft.event;
 
 import nameless.classicraft.api.san.ISanHandler;
 import nameless.classicraft.util.SanUtils;
+import net.minecraft.ChatFormatting;
+import net.minecraft.client.gui.screens.Screen;
+import net.minecraft.network.chat.Component;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.food.FoodProperties;
+import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.Items;
 import net.minecraft.world.item.enchantment.EnchantmentHelper;
 import net.minecraft.world.item.enchantment.Enchantments;
 import net.minecraftforge.event.entity.living.LivingEntityUseItemEvent;
+import net.minecraftforge.event.entity.player.ItemTooltipEvent;
 import net.minecraftforge.eventbus.api.EventPriority;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 
+import java.util.List;
+
 @Mod.EventBusSubscriber
 public class ItemEvent {
+
+    @SubscribeEvent
+    public static void addTooltip(ItemTooltipEvent event) {
+        ItemStack itemStack = event.getItemStack();
+        List<Component> toolTip = event.getToolTip();
+        Item food = itemStack.getItem();
+        FoodProperties foodData = food.getFoodProperties();
+        if (itemStack.isEdible()) {
+            if (!Screen.hasShiftDown()) {
+                toolTip.add(Component.translatable("按下shift键显示更多信息").withStyle(ChatFormatting.ITALIC));
+            } else {
+                if (foodData != null) {
+                    int nutrition = foodData.getNutrition();
+                    float satur = foodData.getSaturationModifier();
+                    toolTip.add(Component.translatable("营养价值:" + nutrition).withStyle(ChatFormatting.WHITE));
+                    toolTip.add(Component.translatable("恢复价值:" + satur).withStyle(ChatFormatting.WHITE));
+                }
+            }
+        }
+    }
 
     @SubscribeEvent(priority = EventPriority.HIGHEST)
     public static void fireBow(LivingEntityUseItemEvent event) {
