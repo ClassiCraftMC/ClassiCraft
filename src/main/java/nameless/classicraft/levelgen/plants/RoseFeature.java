6df78247a334a6ec2e82b0a642d468356f086bad
1798643961 <1798643961@qq.com>
2022-12-10 18:50:09 +0800

    尝试修复

---
+++ src/main/java/nameless/classicraft/levelgen/plants/RoseFeature.java
@@ -0,0 +1,17 @@
+package nameless.classicraft.levelgen.plants;
+
+import net.minecraft.world.level.levelgen.feature.Feature;
+import net.minecraft.world.level.levelgen.feature.RandomPatchFeature;
+import net.minecraft.world.level.levelgen.feature.configurations.RandomPatchConfiguration;
+
+public class RoseFeature extends RandomPatchFeature {
+
+    public RoseFeature() {
+        super(RandomPatchConfiguration.CODEC);
+    }
+
+    public static Feature<?> feature() {
+
+        return null;
+    }
+}
