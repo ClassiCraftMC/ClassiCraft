d283c567ef3b20f1d501933044739d4bdc6d4f10
1798643961 <1798643961@qq.com>
2022-12-15 21:09:15 +0800

    Update

--- common/src/main/java/org/bedracket/classicraft/event/ItemEvents.java
+++ common/src/main/java/org/bedracket/classicraft/event/ItemEvents.java
@@ -8,6 +8,8 @@ import net.minecraft.ChatFormatting;
 import net.minecraft.client.gui.screens.Screen;
 import net.minecraft.network.chat.Component;
 import net.minecraft.world.food.FoodProperties;
+import org.bedracket.classicraft.init.ModBlocks;
+import org.bedracket.classicraft.init.ModItems;
 
 public class ItemEvents {
 
@@ -30,6 +32,15 @@ public class ItemEvents {
                         float satur = foodData.getSaturationModifier();
                         lines.add(Component.translatable("info.classicraft.food.nutrition").append(":" + nutrition).withStyle(ChatFormatting.WHITE));
                         lines.add(Component.translatable("info.classicraft.food.saturation").append(":" + satur).withStyle(ChatFormatting.WHITE));
+                    }
+                }
+            }
+        });
+        ClientTooltipEvent.ITEM.register((stack, lines, flag) -> {
+            if (stack.getTag() != null) {
+                if (stack.is(ModItems.ANDESITE_PEBBLE.get())) {
+                    if (stack.getTag().contains("classicraft:pebble_1")) {
+                        lines.add(Component.translatable(""));
                     }
                 }
             }
