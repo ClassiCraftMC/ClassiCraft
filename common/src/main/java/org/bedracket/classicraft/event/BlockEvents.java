602ac6555aaad66a920a4f702f76823fe4059e28
1798643961 <1798643961@qq.com>
2022-12-15 14:15:56 +0800

    Heap drops and update blockstates

--- common/src/main/java/org/bedracket/classicraft/event/BlockEvents.java
+++ common/src/main/java/org/bedracket/classicraft/event/BlockEvents.java
@@ -6,6 +6,7 @@ import net.minecraft.world.item.Items;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.LeavesBlock;
 import org.bedracket.classicraft.init.ModBlocks;
+import org.bedracket.classicraft.init.ModItems;
 import org.bedracket.classicraft.util.EventUtils;
 
 public class BlockEvents {
@@ -15,6 +16,12 @@ public class BlockEvents {
         EventUtils.blockDropAtOnce(ModBlocks.REAL_WALL_TORCH.get(), Items.STICK);
         EventUtils.blockDropAtOnce(ModBlocks.REAL_SOUL_TORCH.get(), Items.STICK);
         EventUtils.blockDropAtOnce(ModBlocks.REAL_SOUL_WALL_TORCH.get(), Items.STICK);
+        EventUtils.blockDropRandom(ModBlocks.ANDESITE_HEAP.get(), ModItems.ANDESITE_PEBBLE.get(), 4, 8);
+        EventUtils.blockDropRandom(ModBlocks.COBBLESTONE_HEAP.get(), ModItems.COBBLESTONE_PEBBLE.get(), 4, 8);
+        EventUtils.blockDropRandom(ModBlocks.DIORTIE_HEAP.get(), ModItems.DIORTIE_PEBBLE.get(), 4, 8);
+        EventUtils.blockDropRandom(ModBlocks.GRANITE_HEAP.get(), ModItems.GRANITE_PEBBLE.get(), 4, 8);
+        EventUtils.blockDropRandom(ModBlocks.RED_SANDSTONE_HEAP.get(), ModItems.RED_SANDSTONE_PEBBLE.get(), 4, 8);
+        EventUtils.blockDropRandom(ModBlocks.SANDSTONE_HEAP.get(), ModItems.SANDSTONE_PEBBLE.get(), 4, 8);
         BlockEvent.BREAK.register((level, pos, state, player, xp) -> {
             Block block = state.getBlock();
             if (block instanceof LeavesBlock) {
