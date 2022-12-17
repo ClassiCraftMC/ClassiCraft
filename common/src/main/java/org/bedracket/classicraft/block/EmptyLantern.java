293c7e544bf80f3f9a028eb78e64272f60e92dd1
1798643961 <1798643961@qq.com>
2022-12-17 13:59:27 +0800

    空灯笼

---
+++ common/src/main/java/org/bedracket/classicraft/block/EmptyLantern.java
@@ -0,0 +1,8 @@
+package org.bedracket.classicraft.block;
+
+public class EmptyLantern extends AbstractLightBlock {
+
+    public EmptyLantern(Properties pProperties) {
+        super(pProperties);
+    }
+}
