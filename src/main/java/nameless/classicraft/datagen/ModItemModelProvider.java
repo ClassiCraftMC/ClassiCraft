b81522ab88e202526b1c45fa1a822aa9d9e5ba04
1798643961 <1798643961@qq.com>
2023-01-11 16:28:48 +0800

    item datagen

--- src/main/java/nameless/classicraft/datagen/ModItemModelProvider.java
+++ src/main/java/nameless/classicraft/datagen/ModItemModelProvider.java
@@ -1,8 +1,8 @@
 package nameless.classicraft.datagen;
 
 import nameless.classicraft.ClassiCraftMod;
-import nameless.classicraft.init.ModBlocks;
 import net.minecraft.data.DataGenerator;
+import net.minecraft.resources.ResourceLocation;
 import net.minecraftforge.client.model.generators.ItemModelProvider;
 import net.minecraftforge.common.data.ExistingFileHelper;
 
@@ -14,6 +14,13 @@ public class ModItemModelProvider extends ItemModelProvider {
 
     @Override
     protected void registerModels() {
-        basicItem(ModBlocks.SMOOTH_STONE_STAIRS.get().asItem());
+        stairs("smooth_stone_stairs",
+                new ResourceLocation("minecraft:block/stone"),
+                new ResourceLocation("minecraft:block/stone"),
+                new ResourceLocation("minecraft:block/stone"));
+        wallInventory("smooth_stone_wall",
+                new ResourceLocation("minecraft:block/smooth_stone"));
+        wallInventory("stone_wall",
+                new ResourceLocation("minecraft:block/stone"));
     }
 }
