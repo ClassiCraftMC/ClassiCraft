c2910a8d03b469a9da05447277bdef72491a0ec6
TT432 <2437315224@qq.com>
2023-01-11 15:35:20 +0800

    pick block

--- src/main/java/nameless/classicraft/block/VanillaPickBlock.java
+++ src/main/java/nameless/classicraft/block/VanillaPickBlock.java
@@ -1,10 +1,15 @@
 package nameless.classicraft.block;
 
 import nameless.classicraft.util.BlockUtils;
+import net.minecraft.core.BlockPos;
+import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.level.BlockGetter;
 import net.minecraft.world.level.block.SoundType;
+import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.material.Material;
+import net.minecraft.world.phys.HitResult;
 
 public class VanillaPickBlock extends AbstractPickableBlock {
 
@@ -19,5 +24,10 @@ public class VanillaPickBlock extends AbstractPickableBlock {
     @Override
     ItemStack getItem() {
         return vanillaItem.getDefaultInstance();
+    }
+
+    @Override
+    public ItemStack getCloneItemStack(BlockState state, HitResult target, BlockGetter level, BlockPos pos, Player player) {
+        return getItem();
     }
 }
